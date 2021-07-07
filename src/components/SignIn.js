19e813e463c27eab4e359f4c74d5e040f30be15d
Natsuki <ibron1975@gamil.com>
2021-07-08 06:14:30 +0900

    apply grid layout

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -50,7 +50,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState("");
-  console.log(disabled, string);
+  const [isComposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === "";
@@ -75,6 +75,16 @@ export default function SignIn({ setName }) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              console.log({ key: e.key });
+              if (isComposed) return;
+              if (e.key === "Enter") {
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
