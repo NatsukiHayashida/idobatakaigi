3e53edd3cfacb34884eca7bd0d4867860b23fe5e
Natsuki <ibron1975@gamil.com>
2021-07-12 06:09:30 +0900

    fetch messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,40 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { messagesRef } from "../firebase";
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
-  }
-})
+  },
+});
+
+// key: -MeH_pCA2fm83OiSjHmx, value: {name: "なつき", text: "こんんちは"}
+// {key: -MeH_pCA2fm83OiSjHmx, value: name: "なつき", text: "こんんちは"}
+
 const MessageList = () => {
-    const classes = useStyles();
-    return <div className={classes.root}>MessageList</div>
-}
+  const [messages, setMessages] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(3)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val();
+        if (messages === null) return;
+
+        const entries = Object.entries(messages);
+        const newMessages = entries.map((entry) => {
+          // const key = entry[0];
+          // const nameAndText = entry[1];
+          const [key, nameAndText] = entry;
+          return { key, ...nameAndText };
+        });
+        setMessages(newMessages);
+      });
+  }, []);
+  return <div className={classes.root}>MessageList</div>;
+};
 
 export default MessageList;
